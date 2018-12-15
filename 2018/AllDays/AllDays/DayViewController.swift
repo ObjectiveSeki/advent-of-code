52385fc36f7cad22e939b3d7659a3997cfc5c806
Niclas Eriksson <hexadecimal@me.com>
2018-12-15 20:08:01 +0100

    Added loading indication to the view

--- 2018/AllDays/AllDays/DayViewController.swift
+++ 2018/AllDays/AllDays/DayViewController.swift
@@ -14,6 +14,8 @@ class DayViewController: UIViewController {
 
     var day: Day!
     var generator: Generatable!
+    var timer: Timer?
+    var currentAnswer: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +23,48 @@ class DayViewController: UIViewController {
     }
 
     @IBAction func generatePart1Tapped(_ sender: Any) {
+        currentAnswer = answerPart1
+        scheduleTimer()
         let queue = DispatchQueue.init(label: "background", qos: .background)
         queue.async {
             let text = self.generator.generatePartOne(fromFile: self.day.file)
             DispatchQueue.main.async {
                 print(text)
                 self.answerPart1.text = text
+                self.timer?.invalidate()
             }
         }
     }
 
     @IBAction func generatePart2Tapped(_ sender: Any) {
+        currentAnswer = answerPart2
+        scheduleTimer()
         let queue = DispatchQueue.init(label: "background", qos: .background)
         queue.async {
             let text = self.generator.generatePartTwo(fromFile: self.day.file)
             DispatchQueue.main.async {
                 print(text)
                 self.answerPart2.text = text
+                self.timer?.invalidate()
             }
         }
+    }
+
+    func scheduleTimer() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.5,
+            target: self,
+            selector: #selector(fireTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    @objc func fireTimer() {
+        if currentAnswer?.text?.count == 4 {
+            currentAnswer?.text = ""
+        }
+        currentAnswer?.text?.append(".")
     }
 
 }
