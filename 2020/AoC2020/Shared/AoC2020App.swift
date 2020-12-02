c3fe1b28efe8711fc46bff292e0283af67898df3
Niclas Eriksson <hexadecimal@me.com>
2020-12-02 07:37:14 +0100

    Solved day 1

---
+++ 2020/AoC2020/Shared/AoC2020App.swift
@@ -0,0 +1,17 @@
+//
+//  AoC2020App.swift
+//  Shared
+//
+//  Created by Niclas Eriksson on 2020-12-01.
+//
+
+import SwiftUI
+
+@main
+struct AoC2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
